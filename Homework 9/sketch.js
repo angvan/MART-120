127966c1c02ad6ab37da3bf09f3d6e283cf7fdc9
angvan <65193972+angvan@users.noreply.github.com>
2021-03-13 17:29:02 -0700

    Added Homework9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(154,213,237);
+  fill(72,135,161);
+  circle(170,220,450);
+  fill(238,199,154);
+  circle(170,220,350);
+  fill(255,225,191);
+  triangle(270, 275, 298, 220, 330, 275);
+  point(285, 270);
+  point(320, 270);
+  strokeWeight(2)
+  fill(11111);
+  ellipse(230,200,80,50);
+  fill(161,127,88);
+  circle(230,200,50);
+  fill(0);
+  circle(240,200,20);
+  fill(161,127,88)
+  rect(225, 90, 85, 55);
+  circle(285, 80, 50);
+  circle(100, 80, 100);
+  circle(220, 70, 100);
+  circle(50, 180, 70);
+  circle(80, 170, 70);
+  circle(60, 110, 70);
+  circle(150, 70, 70);
+  circle(70, 270, 180);
+  circle(100, 220, 120);
+  circle(200, 120, 70);
+  circle(40, 200, 120);
+  circle(80, 200, 100);
+  circle(60, 150, 100);
+  circle(130, 150, 100);
+  circle(160, 90, 70);
+  circle(80, 170, 70);
+  fill(111111);
+  rect(320,340,75,55);
+  rect(320,10,75,55);
+  fill(249,71,112);
+  textSize(12);
+  text('Living in', 335, 30);
+  text('Circles', 335, 50);
+  fill(000)
+  textSize(18);
+  textStyle(ITALIC);
+  text('Angela', 330, 375)
+
+  line(240, 70, 75, 355);
+  line(240, 70, 25, 255);
+  line(240, 70, 25, 155);
+  line(240, 70, 25, 105);
+
+
+  fill(249,71,112);
+  arc(230, 330, 80, 80, 0, PI + QUARTER_PI, PIE);
+}
